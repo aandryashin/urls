76046568bea905d82b2a0b4901316357782cd441
Alexander Andryashin <aandryashin@gmail.com>
2016-10-01 17:31:14 +0300

    Simple nondistributed implementation with tests

--- urls.go
+++ urls.go
@@ -1,8 +1,86 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
+	"sync"
+)
+
+type jso struct {
+	Url string `json:"url"`
+}
+
+var (
+	mlock  sync.RWMutex
+	urls   map[uint64]string = make(map[uint64]string)
+	lock   sync.Mutex
+	serial uint64
+)
+
+func increment() uint64 {
+	defer func() {
+		lock.Lock()
+		serial++
+		lock.Unlock()
+	}()
+	return serial
+}
+
+func decode(s string) (uint64, error) {
+	return strconv.ParseUint(s, 36, 64)
+}
+
+func encode(i uint64) string {
+	return strconv.FormatUint(i, 36)
+}
+
+func valid(s string) bool {
+	u, err := url.Parse(s)
+	return err == nil && u.IsAbs() && u.Host != ""
+}
 
 func handler() http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.RequestURI() == "/" && r.Method == http.MethodGet {
+			// TODO: write html
+			return
+		}
+		switch r.Method {
+		case http.MethodPost:
+			var o jso
+			json.NewDecoder(r.Body).Decode(&o)
+			r.Body.Close()
+			if !valid(o.Url) {
+				http.Error(w, "Bad Request", http.StatusBadRequest)
+				return
+			}
+			key := increment()
+			mlock.Lock()
+			urls[key] = o.Url
+			mlock.Unlock()
+			json.NewEncoder(w).Encode(jso{encode(key)})
+		case http.MethodGet:
+			uri := strings.TrimPrefix(r.URL.RequestURI(), "/")
+			key, err := decode(uri)
+			if err != nil {
+				http.Error(w, "Bad Request", http.StatusBadRequest)
+				return
+			}
+			mlock.RLock()
+			ru, ok := urls[key]
+			mlock.RUnlock()
+			if !ok {
+				http.Error(w, "Not Found", http.StatusNotFound)
+				return
+			}
+			http.Redirect(w, r, ru, http.StatusMovedPermanently)
+		default:
+			http.Error(w, "Bad Request", http.StatusBadRequest)
+		}
 	})
+	return mux
 }
--- urls_test.go
+++ urls_test.go
@@ -1,11 +1,105 @@
 package main
 
 import (
+	"bytes"
+	"net/http"
+	"net/http/httptest"
+	"strings"
 	"testing"
 
 	. "github.com/aandryashin/matchers"
+	. "github.com/aandryashin/matchers/httpresp"
 )
 
-func TestFunc(t *testing.T) {
-	AssertThat(t, true, Is{true})
+var (
+	srv *httptest.Server
+)
+
+func init() {
+	srv = httptest.NewServer(handler())
+}
+
+func uri(s string) string {
+	return srv.URL + s
+}
+
+func body(s string) *bytes.Reader {
+	return bytes.NewReader([]byte(s))
+}
+
+func TestRootGet(t *testing.T) {
+	resp, err := http.Get(uri("/"))
+	AssertThat(t, err, Is{nil})
+	AssertThat(t, resp, Code{http.StatusOK})
+}
+
+func TestWrongMethod(t *testing.T) {
+	req, _ := http.NewRequest(http.MethodPut, uri("/"), nil)
+	resp, err := http.DefaultClient.Do(req)
+	AssertThat(t, err, Is{nil})
+	AssertThat(t, resp, Code{http.StatusBadRequest})
+}
+
+func TestBadRequest(t *testing.T) {
+	params := []string{
+		`{`,
+		`{}`,
+		`{"url" : ""}`,
+		`{"url" : "https://"}`,
+		`{"url" : "example.com"}`,
+	}
+	for _, param := range params {
+		resp, err := http.Post(uri("/"), "", body(param))
+		AssertThat(t, err, Is{nil})
+		AssertThat(t, resp, Code{http.StatusBadRequest})
+	}
+}
+
+func TestBadKey(t *testing.T) {
+	resp, err := http.Get(uri("/ "))
+	AssertThat(t, err, Is{nil})
+	AssertThat(t, resp, Code{http.StatusBadRequest})
+}
+
+func TestMissingKey(t *testing.T) {
+	resp, err := http.Get(uri("/missing"))
+	AssertThat(t, err, Is{nil})
+	AssertThat(t, resp, Code{http.StatusNotFound})
+}
+
+func TestRedirect(t *testing.T) {
+	serial := increment()
+	mlock.Lock()
+	urls[serial] = "url"
+	mlock.Unlock()
+
+	client := &http.Client{
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			return http.ErrUseLastResponse
+		}}
+
+	resp, err := client.Get(uri("/" + encode(serial)))
+	AssertThat(t, err, Is{nil})
+	AssertThat(t, resp, Code{http.StatusMovedPermanently})
+
+	loc, err := resp.Location()
+	AssertThat(t, err, Is{nil})
+	AssertThat(t, loc.String(), EqualTo{uri("/url")})
+}
+
+func TestNewKey(t *testing.T) {
+	resp, err := http.Post(uri("/"), "", body(`{"url" : "http://example.com"}`))
+	AssertThat(t, err, Is{nil})
+	var o jso
+	AssertThat(t, resp, AllOf{Code{http.StatusOK}, IsJson{&o}})
+
+	k, err := decode(strings.TrimPrefix(o.Url, "/"))
+	AssertThat(t, err, Is{nil})
+
+	mlock.RLock()
+	u, ok := urls[k]
+	mlock.RUnlock()
+
+	AssertThat(t, ok, Is{true})
+	AssertThat(t, u, EqualTo{"http://example.com"})
 }
